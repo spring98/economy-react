69fc7556115a7a73f5f3a332edf99819fe940f17
spring98 <foreverspring98@gmail.com>
2023-03-28 15:49:03 +0900

    init

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Reactsdfsdfsdfsdfds
         </a>
       </header>
     </div>
